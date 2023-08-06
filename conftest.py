a3b2f7c670e57248a05901557aa66497496fe123
YevheniiaMazur <mazurevgenija@gmail.com>
2023-08-07 01:56:45 +0300

    Added tests for DB

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from modules.api.clients.github import GitHub
+from modules.common.database import Database
 
 
 class User:
@@ -30,3 +31,10 @@ def user():
 def github_api():
     api = GitHub()
     yield api
+
+
+@pytest.fixture
+def db():
+    database = Database()
+    yield database
+    database.close_connection()
